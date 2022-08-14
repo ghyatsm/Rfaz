9a5ee52394adaa442551b7eb2680bd3a7d6db576
giyas <gmrenaldy2015@gmail.com>
2022-08-14 19:56:24 +0700

    update2

--- resources/views/layout/v_template.blade.php
+++ resources/views/layout/v_template.blade.php
@@ -12,9 +12,6 @@ if (!isset($_SESSION["username"])) {
 <html lang="en">
 
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>RFAZ Textile | @yield('title')</title>
 
     <!-- Google Font: Source Sans Pro -->
     <link rel="stylesheet" href="{{asset('template')}}/https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
